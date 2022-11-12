8159cfad7db0fcb9bef2f58d08b611042c90522d
tkntkn <kd1049@gmail.com>
2022-11-12 12:01:15 +0900

    BuyボタンとMessage欄の設置

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,23 @@
 import '@testing-library/jest-dom' 
+import userEvent from '@testing-library/user-event' 
 import { render, screen } from '@testing-library/react';
 import { App } from "./App";
 
-it('Hello Worldが書かれている', () => {
-  render(<App />);
-  expect(screen.getByText("Hello World")).toBeInTheDocument();
+describe('vending machine', () => {
+  it('has Buy button', async () => {
+    render(<App />);
+    expect(screen.getByText("Buy")).toBeInTheDocument();
+    await userEvent.click(screen.getByText("Buy"))
+  })
+
+  it('shows "Cola" message when Buy button pushed', async () => {
+    render(<App />)
+    await userEvent.click(screen.getByText("Buy"));
+    expect(screen.getByLabelText("Message")).toHaveTextContent("Cola");
+  })
+
+  it('shows "Please push Buy button." message at first', async () => {
+    render(<App />)
+    expect(screen.getByLabelText("Message")).toHaveTextContent("Please push Buy button.");
+  })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './App.css'
 
 export function App() {
   return (
-    <div className="App">Hello World</div>
+    <div className="App">
+      <button>Buy</button>
+      <p aria-label='Message'>Cola</p>
+    </div>
   )
 }
