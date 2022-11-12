3e92327723a715d4738702659483c29782670535
tkntkn <kd1049@gmail.com>
2022-11-12 11:39:05 +0900

    最小構成にする

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,8 @@
+import '@testing-library/jest-dom' 
 import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
-import { addExcla, App } from "./App";
+import { App } from "./App";
 
-describe("addExcla", () => {
-  it("adds !", () => {
-    expect(addExcla("test message")).toBe("test message!");
-  })
-})
-
-it('Vite + React! が書かれている', () => {
+it('Hello Worldが書かれている', () => {
   render(<App />);
-  expect(screen.getByText("Vite + React!").tagName.toLowerCase()).toBe("h1");
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
 })
