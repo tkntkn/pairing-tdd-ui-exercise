beff74412156ebb3c1608a798db8e11b2b62d584
tkntkn <kd1049@gmail.com>
2022-12-03 10:40:35 +0900

    ボタンを押すとコーラが出る
    Co-authored-by: Yuito Murase <yuito@acupof.coffee>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { useCallback, useState } from 'react'
 import './App.css'
 
 export function App() {
+  const [message, setMessage] = useState("Please push Buy button.")
+  const handleBuyClick = useCallback(() => setMessage("Cola"), [])
+
   return (
     <div className="App">
-      <button>Buy</button>
-      <p aria-label='Message'>Cola</p>
+      <button onClick={handleBuyClick}>Buy</button>
+      <p aria-label='Message'>{message}</p>
     </div>
   )
 }
